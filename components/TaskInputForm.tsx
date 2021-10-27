115326b7ed8d658408b3b133bca08149452f332e
konegishi <k.negishi0705@gmail.com>
2021-10-27 13:46:00 +0900

    [modify-forms][modify]タスク入力画面の要素を修正

--- components/TaskInputForm.tsx
+++ components/TaskInputForm.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
 
 /**
  * TaskInputFormのProps
@@ -37,16 +36,16 @@ const TaskInputForm: React.FC<TaskInputFormProps> = () => {
 
       {/* Collapse */}
       <div
-        className={`shadow absolute bottom-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded ${
-          isOpen ? 'bg-white m-2 py-3 px-6' : 'hidden'
+        className={`shadow absolute bottom-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded-t ${
+          isOpen ? 'bg-white p-2' : 'hidden'
         }`}
       >
         {/* Collapse header */}
-        <div className='block pb-3 mb-4 border-b border-solid border-blueGray-200'>
+        <div className='block mb-4 border-b border-solid border-blueGray-200'>
           <div className='flex flex-wrap'>
             <div className='w-6/12'>
-              <span className='text-left text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm font-bold px-0'>
-                Ape PEX
+              <span className='text-left text-gray-700 mr-0 inline-block whitespace-nowrap text-sm font-bold px-0'>
+                タスクを入力してみましょう
               </span>
             </div>
             <div className='w-6/12 flex justify-end'>
@@ -61,57 +60,18 @@ const TaskInputForm: React.FC<TaskInputFormProps> = () => {
           </div>
         </div>
         {/* Navigation */}
-        <ul className='md:flex-col md:min-w-full flex flex-col list-none'>
-          <li className='items-center'>
-            <Link href='/dashboard'>
-              <a className='text-pink-500 hover:text-pink-600 text-xs uppercase py-3 font-bold block'>
-                <i className='fas fa-tv opacity-75 mr-2 text-sm'></i> Dashboard
-              </a>
-            </Link>
-          </li>
-
-          <li className='items-center'>
-            <Link href='/'>
-              <a className='text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block'>
-                <i className='fas fa-tv opacity-75 mr-2 text-sm'></i> Landing
-                Page
-              </a>
-            </Link>
-          </li>
-
-          <li className='items-center'>
-            <Link href='/'>
-              <a className='text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block'>
-                <i className='fas fa-user-circle text-blueGray-400 mr-2 text-sm'></i>{' '}
-                Profile Page
-              </a>
-            </Link>
-          </li>
-
-          <li className='items-center'>
-            <Link href='/'>
-              <a className='text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block'>
-                <i className='fas fa-fingerprint text-blueGray-400 mr-2 text-sm'></i>{' '}
-                Login
-              </a>
-            </Link>
-          </li>
-
-          <li className='items-center'>
-            <a
-              className='text-blueGray-300 text-xs uppercase py-3 font-bold block'
-              href='#pablo'
-              onClick={(e) => e.preventDefault()}
-            >
-              <i className='fas fa-clipboard-list text-blueGray-300 mr-2 text-sm'></i>{' '}
-              Register (soon)
-            </a>
-          </li>
-
-          <li className='items-center'>
-            <button>ログアウト</button>
-          </li>
-        </ul>
+        <form className='flex flex-row'>
+          <input
+            className='appearance-none border rounded-l py-2 px-3 w-11/12 text-gray-700 border-gray-200 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500'
+            id='taskTitle'
+            type='text'
+            placeholder='新しいタスクの名前'
+          />
+          <button
+            className='fas fa-plus flex-grow-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline'
+            type='button'
+          />
+        </form>
       </div>
     </React.Fragment>
   );
