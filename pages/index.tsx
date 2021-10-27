79e79300bad3be068d509aa22e5b4ad3fa9dcea0
konegishi <k.negishi0705@gmail.com>
2021-10-27 14:15:57 +0900

    [modify-forms][modify]不要なコメントの削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,18 +18,6 @@ export const Home = (): JSX.Element => (
         ]}
       />
       <TaskInputForm />
-      {/* <form className='bg-white rounded-t-xl shadow-lg p-2 flex flex-row'>
-        <input
-          className='appearance-none border rounded-l py-2 px-3 w-11/12 text-gray-700 border-gray-200 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500'
-          id='taskTitle'
-          type='text'
-          placeholder='新しいタスクの名前'
-        />
-        <button
-          className='fas fa-plus flex-grow-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline'
-          type='button'
-        />
-      </form> */}
     </main>
   </div>
 );
