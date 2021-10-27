7142c03b10bd4b684d42f3c61aecc04995ca92a2
konegishi <k.negishi0705@gmail.com>
2021-10-27 12:15:49 +0900

    [add-task][add]Card型を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,47 @@ export const Home = (): JSX.Element => (
     </Head>
 
     <main className='bg-gray-100 flex flex-col min-h-screen'>
+      <span>今日</span>
       <div className='flex-grow pb-4 px-4'>
         <Task title='買い物に行く！！！' />
         <Task title='買い物に行く！！！' />
         <Task title='買い物に行く！！！' />
+      </div>
+      {/* Card型 */}
+      <div className='flex-grow pt-4 px-4'>
+        <div className=' bg-white rounded-xl mb-4 shadow-lg p-2'>
+          <h2 className='ml-2 font-semibold text-2xl text-blueGray-700 mb-2'>
+            今日
+          </h2>
+          <div className='flex flex-row items-center border-t border-gray-200 m-2'>
+            <input
+              type='checkbox'
+              className='h-5 w-5 rounded m-2 border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500'
+            />
+            <span className='text-xl text-blueGray-700'>買い物に行く</span>
+          </div>
+          <div className='flex flex-row items-center border-t border-gray-200 m-2'>
+            <input
+              type='checkbox'
+              className='h-5 w-5 rounded m-2 border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500'
+            />
+            <span className='text-xl text-blueGray-700'>買い物に行く</span>
+          </div>
+          <div className='flex flex-row items-center border-t border-gray-200 m-2'>
+            <input
+              type='checkbox'
+              className='h-5 w-5 rounded m-2 border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500'
+            />
+            <span className='text-xl text-blueGray-700'>買い物に行く</span>
+          </div>
+          <div className='flex flex-row items-center border-t border-gray-200 m-2'>
+            <input
+              type='checkbox'
+              className='h-5 w-5 rounded m-2 border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500'
+            />
+            <span className='text-xl text-blueGray-700'>買い物に行く</span>
+          </div>
+        </div>
       </div>
       <div>
         <form className='bg-white rounded-t-xl shadow-lg p-2 flex flex-row'>
