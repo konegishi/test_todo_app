6ba86bffa003c52ca7adccc072338653e0d67beb
konegishi <k.negishi0705@gmail.com>
2021-10-30 12:00:58 +0900

    [modify-forms][modify]Taskカードの細かい修正

--- components/Task.tsx
+++ components/Task.tsx
@@ -18,10 +18,10 @@ export interface TaskProps {
 const Task: React.FC<TaskProps> = (props) => {
   return (
     <React.Fragment>
-      <div className='flex flex-row items-center border-t border-gray-200 mb-2'>
+      <div className='flex flex-row items-center border-t border-gray-200 mb-2 pt-2'>
         <input
           type='checkbox'
-          className='h-5 w-5 rounded m-2 border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500'
+          className='h-5 w-5 form-checkbox rounded-full mx-2 border-gray-300 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 text-indigo-500'
         />
         <span className='text-xl text-blueGray-700'>{props.name}</span>
       </div>
--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -20,8 +20,8 @@ const TaskCard: React.FC<TaskCardProps> = (props) => {
   return (
     <React.Fragment>
       <div className='flex-grow pt-4 px-4'>
-        <div className=' bg-white rounded-xl mb-4 shadow-lg p-2'>
-          <h2 className='ml-2 font-semibold text-2xl text-blueGray-700 mb-2'>
+        <div className=' bg-white rounded mb-4 shadow-lg p-2'>
+          <h2 className='ml-2 mb-2 font-semibold text-2xl text-blueGray-700'>
             今日
           </h2>
           {props.tasks.map((task, index) => {
