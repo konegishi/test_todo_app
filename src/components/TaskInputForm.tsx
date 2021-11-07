cdd8b97771d5658f1d2e408f936826bd40fc7cbe
konegishi <k.negishi0705@gmail.com>
2021-11-07 23:16:10 +0900

    [add-toggle-complete][modify]少しだけ修正

--- src/components/TaskInputForm.tsx
+++ src/components/TaskInputForm.tsx
@@ -33,60 +33,57 @@ const TaskInputForm: React.FC<TaskInputFormProps> = (props) => {
     setOpen(!isOpen);
   };
 
-  if (isOpen) {
-    return (
-      <React.Fragment>
-        {/* blackOverlay */}
-        <div className='absolute w-full min-h-full'>
-          <span
-            id='blackOverlay'
-            className='absolute w-full min-h-full bg-gray-500 bg-opacity-75'
-          ></span>
-        </div>
-        {/* タスク入力フォーム */}
-        <div
-          className={`shadow absolute bottom-0 left-0 right-0 z-40 items-center flex-1 rounded-t bg-white p-2 sticky${
-            !isOpen && ' hidden'
-          }`}
-        >
-          <div className='block mb-2'>
-            <div className='flex flex-row items-center'>
-              <div className='w-6/12'>
-                <span className='text-left text-gray-700 mr-0 inline-block whitespace-nowrap text-sm font-bold px-0'>
-                  新しいタスクを入力してみましょう
-                </span>
-              </div>
-              <div className='w-6/12 flex justify-end'>
-                <button
-                  type='button'
-                  className='cursor-pointer text-black opacity-50 px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent'
-                  onClick={() => setOpen(!isOpen)}
-                >
-                  <i className='fas fa-times'></i>
-                </button>
-              </div>
+  return isOpen ? (
+    <React.Fragment>
+      {/* blackOverlay */}
+      <div className='absolute w-full min-h-full'>
+        <span
+          id='blackOverlay'
+          className='absolute w-full min-h-full bg-gray-500 bg-opacity-75'
+        ></span>
+      </div>
+      {/* タスク入力フォーム */}
+      <div
+        className={`shadow absolute bottom-0 left-0 right-0 z-40 items-center flex-1 rounded-t bg-white p-2 sticky${
+          !isOpen && ' hidden'
+        }`}
+      >
+        <div className='block mb-2'>
+          <div className='flex flex-row items-center'>
+            <div className='w-6/12'>
+              <span className='text-left text-gray-700 mr-0 inline-block whitespace-nowrap text-sm font-bold px-0'>
+                新しいタスクを入力してみましょう
+              </span>
+            </div>
+            <div className='w-6/12 flex justify-end'>
+              <button
+                type='button'
+                className='cursor-pointer text-black opacity-50 px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent'
+                onClick={() => setOpen(!isOpen)}
+              >
+                <i className='fas fa-times'></i>
+              </button>
             </div>
           </div>
-          {/* Form */}
-          <form className='flex flex-row'>
-            <input
-              className='appearance-none border rounded-l py-2 px-3 w-11/12 text-gray-700 border-gray-200 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500'
-              id='taskTitle'
-              ref={inputRef}
-              type='text'
-              placeholder='新しいタスクの名前'
-            />
-            <button
-              className='fas fa-arrow-circle-up flex-grow-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline'
-              type='button'
-              onClick={() => handleOnClickAddButton()}
-            />
-          </form>
         </div>
-      </React.Fragment>
-    );
-  }
-  return (
+        {/* Form */}
+        <form className='flex flex-row'>
+          <input
+            className='appearance-none border rounded-l py-2 px-3 w-11/12 text-gray-700 border-gray-200 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500'
+            id='taskTitle'
+            ref={inputRef}
+            type='text'
+            placeholder='新しいタスクの名前'
+          />
+          <button
+            className='fas fa-arrow-circle-up flex-grow-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline'
+            type='button'
+            onClick={() => handleOnClickAddButton()}
+          />
+        </form>
+      </div>
+    </React.Fragment>
+  ) : (
     <React.Fragment>
       <div className='flex justify-center shadow-2xl sticky bottom-0'>
         <button
